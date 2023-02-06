06284e6cd873d331ab8639e7ac0f6ad71902f9b9
longo2002 <lombardo.fra2002@gmail.com>
2023-02-06 15:52:20 +0100

    atempt milestone 2

---
+++ js/app.js
@@ -0,0 +1,29 @@
+console.log("works!")
+
+let arrowLeft = document.getElementsByClassName('.carosello__arrow.arrow-left')
+let arrowRight = document.getElementsByClassName('.carosello__arrow.arrow-right');
+let slides = [
+	'./img/01.jpg',
+	'./img/02.jpg',
+	'./img/03.jpg',
+	'./img/04.jpg',
+	'./img/05.jpg',
+]
+
+let indiceSlideAttiva = 0
+const slideElements = document.getElementsByClassName('slide')
+console.log(slideElements)
+
+
+arrowLeft.addEventListener ( "click",
+
+    // function myFunction() {
+
+    //     let firstSlide = slideElements[0]
+    //     firstSlide.classList.remove('active')
+
+    //     let secondSlide = slideElements[1]
+    //     secondSlide.classList.add('active')
+
+    // }
+)   
